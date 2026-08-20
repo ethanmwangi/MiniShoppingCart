f6716869d3aff34377926da765b8b76e8e0c66e1
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2026-08-20 22:29:23 +0300

    Complete shopping cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,67 @@
 import ProductCard from "./ProductCard";
 import { useState } from "react";
-const cart = [];
+
 const products = [
   {
     id: 1,
-    name: "Nike Air max",
-    price: 1200
+    name: "Nike Air Max",
+    price: 1200,
   },
   {
     id: 2,
     name: "Adidas Samba",
-    price: 1500
+    price: 1500,
   },
   {
     id: 3,
     name: "Puma Suede",
-    price: 1000
-  }
+    price: 1000,
+  },
 ];
+
 function App() {
   const [cart, setCart] = useState([]);
+
   function handleClick(product) {
     setCart([...cart, product]);
     console.log(product);
   }
+
+  function removeFromCart(removeId) {
+    const newCart = cart.filter((item) => item.id !== removeId);
+    setCart(newCart);
+  }
+
   const productCards = products.map((product) => {
     return (
       <ProductCard
         key={product.id}
         name={product.name}
         price={product.price}
-        onAddToCart={() => handleClick(product)}  //When the button gets clicked, run handleClick and give it THIS produc//
+        onAddToCart={() => handleClick(product)}
       />
     );
   });
+
   return (
     <div>
       {productCards}
-      <p>Cart:{cart.length}</p> // to tell us how many items aree in the new array//
-      {cart.map((item) => {
-       return (
-        <p key={item.id}>{item.name} - ${item.price}</p>
+
+      <p>Cart: {cart.length}</p>
+
+      {cart.map((item, index) => {
+        return (
+          <div key={`${item.id}-${index}`}>
+            <p>
+              {item.name} - KES {item.price}
+            </p>
+
+            <button onClick={() => removeFromCart(item.id)}>
+              Remove
+            </button>
+          </div>
         );
-      })};
+      })}
     </div>
   );
 }
